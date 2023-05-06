c2a15cc81b45d905cff0d9037ae3bf20f09f83ee
abrar-nazib <abrarnazib@gmail.com>
2023-05-07 00:02:18 +0600

    task-4 partially done

--- routes/uploadRoute.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-
-const { uploadProfilePics } = require("../controllers/uploadController");
-const upload = require("../middleware/uploadMiddleware");
-
-router.post("/profilePics", upload.single("gifUrl"), uploadProfilePics);
-
-module.exports = router;
--- utils/validationErrorFormatter.js
+++
@@ -1,4 +0,0 @@
-const errorFormatter = (error) => error.msg;
-// not error.message because we are using express-validator
-
-module.exports = errorFormatter;
