d304fa1cc5c7ac248614bfeb6601d6d3aba7c6ef
abrar-nazib <abrarnazib@gmail.com>
2023-05-08 00:52:44 +0600

    charts.js study needed

--- design1/scripts.js
+++ design1/scripts.js
@@ -2,3 +2,25 @@ $(".sidebar ul li").on("click", function () {
   $(".sidebar ul li.active").removeClass("active");
   $(this).addClass("active");
 });
+const ctx = document.getElementById("myChart");
+
+new Chart(ctx, {
+  type: "bar",
+  data: {
+    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+});
